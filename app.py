5befc6e244a19f7c9b2020c71aca13032c5eb057
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-09-21 08:42:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import sys
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+            return num1 + num2
+    elif operation == "sub":
+            return num1 - num2
+    elif operation == "mul":
+            return num1 * num2
+    elif operation == "div":
+        return num1 / num2
+    else:
+            return "Invalid operations"
+
+# Get input values from command line arguments
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+operation = sys.argv[3]
+
+result = perform_operation(num1, num2, operation)
+print("Result:", result)
