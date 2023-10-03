3091c3899887d4a207e699a4de39aa9f73a37258
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-10-03 08:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "mul":
             return num1 * num2
     elif operation == "div":
-        return num1 / num2
+        return num1 / num2 - 1
     else:
             return "Invalid operation"
 
